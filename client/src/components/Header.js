0f6f06a4c5c200b945768860195e2babc5408f16
MKYRENE <rodriguezm90@icloud.com>
2023-08-10 01:23:56 -0400

    additional styling  on main page.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import logoImage from "../assets/logo.png"; // Update the path to your logo image
 
 function Header(propsObj) {
   const navLinkStyles = {
@@ -8,7 +9,9 @@ function Header(propsObj) {
 
   return (
     <header className="row justify-between align-center">
-      <h3>Logo</h3>
+      {/* Logo image */}
+      <img src={logoImage} alt="Logo" style={{ width: '100px', height: 'auto' }} />
+
       <nav className="nav-links">
         <NavLink to="/" style={{ color: 'red' }}>Home</NavLink>
         <NavLink to="/portfolio" style={{ color: 'red' }}>Portfolio</NavLink>
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -4,13 +4,16 @@ function Main() {
   return (
     <main>
       <section className="landing-intro">
-        <h1 className="intro-text">CodeCanvas: 
-        Unveiling a Journey in Web Development</h1>
-        <p>
+        <div className="centered-content">
+          <h1 className="intro-text">CodeCanvas: 
+            Unveiling a Journey in Web Development
+          </h1>
+        </div>
+        {/* <p>
           Hi, I'm Michael Rodriguez, a passionate web developer. I love
           building innovative and user-friendly web applications. Explore my
           projects below!
-        </p>
+        </p> */}
       </section>
       {/* ... */}
     </main>
