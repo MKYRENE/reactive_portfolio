a56a5cc9a594e9c4e6ccbf1476508c2307c8c692
MKYRENE <rodriguezm90@icloud.com>
2023-08-10 03:42:06 -0400

    updated scss and readME file.

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,15 +1,21 @@
+// Import the travel image from the assets folder
+import travel from '../assets/travel.jpg';
 import React from 'react';
 
 function Portfolio() {
     return (
         <main>
+            {/* Portfolio Title */}
             <h1 className="portfolio-title animated-h1">Project Portfolio</h1>
 
+            {/* Placeholder for other content */}
             <p className="indented-paragraph">
                 {/* ... */}
             </p>
-            <section className="projects">
 
+            {/* Projects Section */}
+            <section className="projects">
+                {/* Pomofocus Project */}
                 <div className="project-card">
                     <div className="image-container">
                         <img
@@ -18,9 +24,7 @@ function Portfolio() {
                             alt="Pomofocus App"
                         />
                     </div>
-
                     <div className="project-info">
-
                         <p className="project-description">
                             {/* ... */}
                         </p>
@@ -28,6 +32,33 @@ function Portfolio() {
                             <a
                                 className="github-button"
                                 href="https://github.com/mkyrene/pomo_focus"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                GitHub Repository
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Travel Circle Project */}
+                <div className="project-card">
+                    <div className="image-container">
+                        <img
+                            className="project-image smaller-image"
+                            src={travel} // Importing travel variable for image source
+                            alt="Travel Circle App"
+                        />
+                    </div>
+                    <div className="project-info">
+                        <h3 className="project-title">Travel Circle App</h3>
+                        <p className="project-description">
+                            {/* ... */}
+                        </p>
+                        <div className="button-container">
+                        <a
+                                className="github-button"
+                                href="https://github.com/MKYRENE/travel_circle_app"
                                 target="_blank"
                                 rel="noopener noreferrer"
                             >
